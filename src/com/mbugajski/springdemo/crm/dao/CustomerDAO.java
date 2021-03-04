a1b075eaa8593a0ad5453b80efaf8100377498ac
MBugajski <michalbugajski@outlook.com>
2021-03-04 14:48:30 +0100

    Setup a DAO pattern for Customer class

---
+++ src/com/mbugajski/springdemo/crm/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.mbugajski.springdemo.crm.dao;
+
+import java.util.List;
+
+import com.mbugajski.springdemo.crm.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
