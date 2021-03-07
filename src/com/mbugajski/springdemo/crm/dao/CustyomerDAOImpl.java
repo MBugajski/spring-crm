6587149047d7e46ff1b1c836368eca5090de332b
MBugajski <michalbugajski@outlook.com>
2021-03-07 12:32:22 +0100

    Implemented saving a Customer into a database

--- src/com/mbugajski/springdemo/crm/dao/CustyomerDAOImpl.java
+++ src/com/mbugajski/springdemo/crm/dao/CustyomerDAOImpl.java
@@ -2,13 +2,11 @@ package com.mbugajski.springdemo.crm.dao;
 
 import java.util.List;
 
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.mbugajski.springdemo.crm.entity.Customer;
 
@@ -17,17 +15,25 @@ public class CustyomerDAOImpl implements CustomerDAO {
 
 	@Autowired
 	private SessionFactory sessionFactory;
-	
+
 	@Override
 	public List<Customer> getCustomers() {
-		
+
 		Session currentSession = sessionFactory.getCurrentSession();
-		
-		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
-		
+
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName", Customer.class);
+
 		List<Customer> customers = theQuery.getResultList();
-		
+
 		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		currentSession.save(theCustomer);
 	}
 
 }
