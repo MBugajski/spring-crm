6a1802c4ec758095ab11965d0d1b2e2f0a4f3c32
MBugajski <michalbugajski@outlook.com>
2021-03-19 23:49:39 +0100

    Added @Before logging advice

--- src/main/java/com/mbugajski/springdemo/crm/aspect/CRMLoggingAspect.java
+++ src/main/java/com/mbugajski/springdemo/crm/aspect/CRMLoggingAspect.java
@@ -2,17 +2,26 @@ package com.mbugajski.springdemo.crm.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
-
-
 
 @Aspect
 @Component
 public class CRMLoggingAspect {
-	
-	private Logger myLogger = Logger.getLogger(getClass().getName());
-	
-	
 
+	private Logger myLogger = Logger.getLogger(getClass().getName());
+
+	@Before("com.mbugajski.springdemo.crm.aspect.expressions.CRMExpressions.forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+
+		myLogger.info("=====> in @Before: calling method: " + theJoinPoint.getSignature().toShortString());
+
+		Object[] args = theJoinPoint.getArgs();
+
+		for (Object arg : args) {
+			myLogger.info("=====> argument: " + arg.toString());
+		}
+	}
 }
