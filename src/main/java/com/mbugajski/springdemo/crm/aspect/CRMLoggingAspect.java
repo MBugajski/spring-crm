dce67b80152ea22062209839e9d3ac5a4b6be5ea
MBugajski <michalbugajski@outlook.com>
2021-03-20 22:52:08 +0100

    Added @AfterReturning logging advice

--- src/main/java/com/mbugajski/springdemo/crm/aspect/CRMLoggingAspect.java
+++ src/main/java/com/mbugajski/springdemo/crm/aspect/CRMLoggingAspect.java
@@ -3,6 +3,7 @@ package com.mbugajski.springdemo.crm.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -23,5 +24,16 @@ public class CRMLoggingAspect {
 		for (Object arg : args) {
 			myLogger.info("=====> argument: " + arg.toString());
 		}
+	}
+	
+	@AfterReturning(
+			pointcut="com.mbugajski.springdemo.crm.aspect.expressions.CRMExpressions.forAppFlow()",
+			returning="theResult"
+			)
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		
+		myLogger.info("=====> in @AfterReturning: from method: " + theJoinPoint.getSignature().toShortString());
+		
+		myLogger.info("=====> result: " + theResult);
 	}
 }
