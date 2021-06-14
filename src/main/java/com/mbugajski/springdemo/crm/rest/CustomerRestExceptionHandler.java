fc2fb9cee0f71ea712ed0451e96271cf540b1346
MBugajski <michalbugajski@outlook.com>
2021-06-14 23:52:40 +0200

    Created 404 exception handler for Customer class

---
+++ src/main/java/com/mbugajski/springdemo/crm/rest/CustomerRestExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.mbugajski.springdemo.crm.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomerRestExceptionHandler {
+	
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleNotFoundException(CustomerNotFoundException exc) {
+
+		CustomerErrorResponse errorResponse = new CustomerErrorResponse();
+		
+		errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+		errorResponse.setMessage(exc.getMessage());
+		errorResponse.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+	}
+
+}
